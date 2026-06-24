6992e4637c405916ece7f50c1ee10b7bd53efa57
Diamond975-py <giulio97gentile@gmail.com>
2026-06-24 17:26:00 +0200

    Aggiunto API Gateway di base

---
+++ api.tf
@@ -0,0 +1,70 @@
+
+resource "aws_api_gateway_rest_api" "api" {
+    name = "rest-api"
+}
+
+resource "aws_api_gateway_resource" "test" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    parent_id = aws_api_gateway_rest_api.api.root_resource_id
+    path_part = "test"
+} 
+
+resource "aws_api_gateway_method" "get_test" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    resource_id = aws_api_gateway_resource.test.id
+    http_method = "GET"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+  rest_api_id          = aws_api_gateway_rest_api.api.id
+  resource_id          = aws_api_gateway_resource.test.id
+  http_method          = aws_api_gateway_method.get_test.http_method
+  type                 = "MOCK"
+  timeout_milliseconds = 29000
+
+  request_parameters = {
+    "integration.request.header.X-Authorization" = "'static'"
+  }
+
+    request_templates = {
+    "application/json" = <<EOF
+    {
+    "statusCode": 200
+    }
+    EOF
+    }
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.get_test.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.get_test.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
+  response_templates = {
+    "application/json" = jsonencode({
+      message = "Test completato"
+      status  = "ok"
+    })
+  }
+
+  depends_on = [aws_api_gateway_integration.MyDemoIntegration]
+}
+
+resource "aws_api_gateway_deployment" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deploy.id
+  stage_name    = "prod"
+}
